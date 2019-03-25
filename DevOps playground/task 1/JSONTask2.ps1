6305dfa7ab8cb3f7885d0327157ecc20a25406d9
ACCACNH <sjurikek@gmail.com>
2019-03-25 16:06:56 +0200

    Add files via upload

---
+++ DevOps playground/task 1/JSONTask2.ps1
@@ -0,0 +1,15 @@
+﻿param (
+    [CmdletBinding()]
+    [Parameter(
+        Mandatory = $true,
+        HelpMessage = "Please enter the path to .json file",
+        Position = 1
+    )][string]$Path
+)
+
+$jsondata = Get-Content -Path $Path -Raw | ConvertFrom-Json 
+for ($i = 0; $i -lt $jsondata.Count; $i++) {
+    $jsondata[$i]
+}
+
+
---
+++ DevOps playground/task 1/XMLTask.ps1
@@ -0,0 +1,31 @@
+﻿
+param (
+    [CmdletBinding()]
+    [Parameter(
+        Mandatory=$true,
+        HelpMessage ="Please enter the path where .xml lies",
+        Position=1
+        )][string]$Path,
+    [Parameter(
+        Mandatory=$true,
+        HelpMessage="Please, specify the existing .xml file name",
+        Position = 2
+        )][string]$FileName,
+    [Parameter(
+        Mandatory=$true,
+        HelpMessage="Please, specify the save path of .xml files",
+        Position=3)][string]$SavePath
+)
+$xmlFullPath = ("{0}{1}" -f $Path, $FileName)
+$copyCount = 10;
+
+$xml = New-Object XML
+$xml.Load($xmlFullPath)
+
+for ($i = 1; $i -le $copyCount; $i++) {  
+    foreach ($element in $xml.SelectNodes("//th")) {
+        $element.InnerText = "test" + $i.ToString()
+    } 
+    $xmlSavePath = ("{0}\{1}" -f $SavePath, $i.ToString())
+    $xml.Save($xmlSavePath+".XML")
+}
